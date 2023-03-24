38111e76c8c5cbd53ee5eefaa61e14cc66ff274e
Vladimir Latypov <donrumata03@gmail.com>
2023-03-24 22:38:48 +0300

    Start matrix generator

--- core/matrix_generator.py
+++ core/matrix_generator.py
@@ -1,3 +1,4 @@
+from random import random
 from typing import Callable, NamedTuple
 
 import numpy as np
@@ -8,7 +9,25 @@ class QuadraticForm(NamedTuple):
     matrix: np.ndarray
 
     def __call__(self, x: np.ndarray):
-        return x[:, newaxis] @ self.matrix @ x
+        return x @ self.matrix @ x[:, newaxis]
+
+
+def random_basis(n):
+    pass
+
+
+def canonical_basis(n):
+    return np.eye(n)
+
+
+def generate_positive_definite_quadratic_form(dimensions, condition_number, basis_producer=None):
+    assert condition_number >= 1
+    assert dimensions >= 2
+    smallest_eigenvalue = 1.
+    greatest_eigenvalue = smallest_eigenvalue * condition_number
+
+    eigenvalues = [smallest_eigenvalue] + [random() for _ in range(dimensions - 2)] + [greatest_eigenvalue]
+
 
 def evaluator(computation: Callable[[_], int], n_times):
     pass
