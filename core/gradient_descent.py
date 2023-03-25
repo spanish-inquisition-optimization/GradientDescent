8301305d1f1bce6edc182982fc7153b76adeb2cf
Vladimir Latypov <donrumata03@gmail.com>
2023-03-26 00:16:45 +0300

    Show that GD fails to converge for even canonical at k >= 50

--- core/gradient_descent.py
+++ core/gradient_descent.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import scipy.constants
 from numpy import newaxis
 
-precision = 1e-5
+precision = 1e-3
 
 
 def gradient_descent(target_function: Callable[[np.ndarray], float],
