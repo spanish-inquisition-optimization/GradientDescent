580e10fc52aa3e6b8858f3d266b4f3828d6b4b71
Vladimir Latypov <donrumata03@gmail.com>
2023-04-15 13:07:09 +0300

    feature(bonus): + line searchers comparison

--- core/visualizer.py
+++ core/visualizer.py
@@ -1,10 +1,11 @@
-from typing import Tuple, NamedTuple, Callable
+from typing import Tuple, NamedTuple, Callable, List
 
 import numpy as np
 from matplotlib import pyplot as plt
 from numpy import newaxis
 
-from core.gradient_descent import symmetric_gradient_computer, gradient_descent
+from core.gradient_descent import symmetric_gradient_computer, gradient_descent, precision
+from core.optimizer_evaluator import n_calls_mocker, generate_positive_definite_quadratic_form, random_normalized_vector
 
 
 class SearchRegion2d(NamedTuple):
@@ -105,7 +106,8 @@ def plot_section_graphs(f, discrete_param_values, continuous_param_values):
         plots[i].grid()
 
 
-def test_linear_search(linear_search: Callable[[Callable[[float], float], Callable[[float], float]], float]):
+def test_linear_search(linear_search: Callable[[Callable[[float], float], Callable[[float], float]], float],
+                       measure_performance=False):
     alpha = 5
 
     def quadratic(x):
@@ -126,6 +128,7 @@ def test_linear_search(linear_search: Callable[[Callable[[float], float], Callab
     rosen_roi = SearchRegion2d((-2, 1), (-1, 1))
 
     class SearchSetup(NamedTuple):
+        name: str
         f: Callable[[np.ndarray], float]
         gradient: Callable[[np.ndarray], np.ndarray]
         roi: SearchRegion2d
@@ -133,17 +136,47 @@ def test_linear_search(linear_search: Callable[[Callable[[float], float], Callab
         start_point: np.ndarray
         true_minimum: float = None
 
-    setups = [SearchSetup(quadratic, quadratic_derivative, quadratic_roi, 50, np.array([-20, -20]), 0),
-              SearchSetup(trig, trig_derivative, trig_roi, 100, np.array([-0.1, -0.4])),
-              SearchSetup(rosen, rosen_derivative, rosen_roi, 20, np.array([-1.5, 0.25]), 0)
+    setups = [SearchSetup("Quadratic", quadratic, quadratic_derivative, quadratic_roi, 50, np.array([-20, -20]), 0),
+              SearchSetup("Trigonometric", trig, trig_derivative, trig_roi, 100, np.array([-0.1, -0.4])),
+              SearchSetup("Rosenbrock", rosen, rosen_derivative, rosen_roi, 20, np.array([-1.5, 0.25]), 0)
               ]
+    if measure_performance:
+        dims = 100
+        k = 100
+        mvf = generate_positive_definite_quadratic_form(dims, k)
+        setups.append(SearchSetup(f"{dims}-dimensional quadratic with k = {k}", mvf, mvf.gradient_function(), None, 1000, random_normalized_vector(dims), 0))
 
     for setup in setups:
-        visualize_function_3d(setup.f, setup.roi)
-        visualize_optimizing_process(setup.f, setup.roi,
-                                     np.array(gradient_descent(
-                                         setup.f,
-                                         setup.gradient,
-                                         setup.start_point,
-                                         linear_search,
-                                         lambda f, points: len(points) > setup.num_steps)), setup.true_minimum)
+        resultant_true_minimum = setup.true_minimum
+
+        if not measure_performance:
+            visualize_function_3d(setup.f, setup.roi)
+
+        if measure_performance and resultant_true_minimum is None:
+            resultant_true_minimum = setup.f(gradient_descent(
+                setup.f, setup.gradient, setup.start_point, linear_search,
+                lambda f, points: len(points) > setup.num_steps
+            )[-1])
+            assert np.linalg.norm(resultant_true_minimum) < 1e5
+
+        def terminate_condition(f: Callable[[np.ndarray], float], points: List[np.ndarray]) -> bool:
+            have_true_minimum = resultant_true_minimum is not None
+            return (have_true_minimum and f(points[-1]) - resultant_true_minimum < precision) \
+                or len(points) > setup.num_steps
+
+        mocked_f = n_calls_mocker(setup.f)
+        mocked_grad = n_calls_mocker(setup.gradient)
+
+        trajectory = gradient_descent(
+            mocked_f, mocked_grad, setup.start_point, linear_search,
+            terminate_condition
+        )
+
+        if measure_performance:
+            if np.linalg.norm(trajectory[-1]) > 1e7:
+                print(f"GD diverged at {setup.name} (got vector norm {np.linalg.norm(trajectory[-1])} after {len(trajectory)} steps), such a pity…")
+            else:
+                print(
+                f"{setup.name} : f called {mocked_f.n_calls} times, f' called {mocked_grad.n_calls} times, total score: {mocked_f.n_calls + mocked_grad.n_calls}")
+        else:
+            visualize_optimizing_process(setup.f, setup.roi, np.array(trajectory), resultant_true_minimum)
